e7f9b71644e991d99e4f6774f581da208bfcb102
Stanislav Minakov <sminakov@snapchat.com>
2017-07-25 22:30:01 +0300

    Compile time check for opengl es

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include "MainWindow.h"
+
 #include <QApplication>
-#include <QOpenGLContext>
-#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication::setStyle("fusion");
 
-    QApplication a(argc, argv);
-
     QSurfaceFormat format;
     format.setDepthBufferSize(24);
-    if(QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL)
-    {
-        format.setVersion(3, 3);
-        format.setProfile(QSurfaceFormat::CoreProfile);
-    }
-    else
-    {
-        format.setVersion(3, 0);
-    }
+#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
+    format.setVersion(3, 0);
+#else
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+#endif
     QSurfaceFormat::setDefaultFormat(format);
+
+    QApplication a(argc, argv);
 
     MainWindow w;
     w.show();
